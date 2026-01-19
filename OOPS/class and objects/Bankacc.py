3731c772810fcf99fd42fdfd902a439147b0be25
avk0004 <vric.vignesh@gmail.com>
2026-01-19 23:09:23 +0530

    Bank deposit and withdrwal with class and static method for better understanding

---
+++ OOPS/class and objects/Bankacc.py
@@ -0,0 +1,28 @@
+class Bank:
+    def __init__(self,BankName,Balance):
+        self.BName = BankName
+        self.bal = Balance 
+    def deposit(self,dep):
+        self.bal += dep
+        return self.bal
+    def withdrawal(self,withd):
+        self.bal -= withd
+        return self.bal
+    @classmethod
+    def changeName(cls,BankName,balance):
+        return cls(BankName,balance)
+    @staticmethod
+    def Bank_policy(bal):
+        if bal>=100:
+            print("Bank Polciy")
+Name= 'abc'
+balance = 5000
+bank = Bank(Name,balance)
+d= bank.deposit(500)
+# Bank Deposit is 500 so 5000+500 = 5500
+print(d)
+w = bank.withdrawal(100)
+# with drawal is 100 so 5500-100 = 5400
+print(w)
+x = bank.changeName('xyz',4000)
+print(x.BName)
